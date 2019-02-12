b732fd64f496da59d8a45db889e8f7bfe2191fd1
meng <daniel.zhou@clickdishes.com>
2019-02-11 22:19:20 -0700

    check

--- index.js
+++ index.js
@@ -1,18 +1,10 @@
 const express = require("express");
-const bodyParser = require("body-parser");
-const cors = require("cors");
-
-require("dotenv").config();
 
 const app = express();
-
-app.use(cors());
-app.use(bodyParser.json());
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
-require("./routes/employee-routes")(app);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
